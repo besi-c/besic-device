a26798188e0830eb4f969b6f96b2a9115b1b2746
Penn Bauman <pennbauman@protonmail.com>
2021-06-25 14:30:28 -0400

    added password

--- heartbeat.sh
+++ heartbeat.sh
@@ -3,24 +3,32 @@
 #   https://github.com/pennbauman/besic-relay
 #   Penn Bauman <pcb8gb@virginia.edu>
 
-url="http://dev.remote.besic.org"
+dir="/var/besic"
 log="/var/log/besic/heartbeat.log"
 
-mkdir -p $(dirname $log)
 
-dir="/var/besic"
-config="$dir/config.toml"
-
-id=$(tq .mac $config)
+url=$(tq .remote $dir/urls.toml)
 if (($? != 0)); then
-	echo "Config error"
+	echo "[$(date --rfc-3339=seconds)]: Url not found (remote)" >> $log
 	exit 1
 fi
+
+id=$(tq .mac $dir/config.toml)
+if (($? != 0)); then
+	echo "[$(date --rfc-3339=seconds)]: Config not found (mac)" >> $log
+	exit 1
+fi
+password=$(tq .password $dir/config.toml)
+if (($? != 0)); then
+	echo "[$(date --rfc-3339=seconds)]: Config not found (password)" >> $log
+	exit 1
+fi
+
 
 fail=0
 i=0
 while (( $i < 9 )); do
-	curl "$url/api/device/$id/heartbeat"
+	curl "$url/api/device/$id/heartbeat" -d "password=$password"
 	if (( $? != 0 )); then
 		fail=$(($fail + 1))
 	fi
@@ -29,9 +37,9 @@ while (( $i < 9 )); do
 done
 
 if (($fail > 1)); then
-	echo "[$(date)] $fail heartbeats failed" >> $log
+	echo "[$(date --rfc-3339=seconds)] $fail heartbeats failed" >> $log
 elif (($fail > 0)); then
-	echo "[$(date)] 1 heartbeat failed" >> $log
+	echo "[$(date --rfc-3339=seconds)] 1 heartbeat failed" >> $log
 else
 	if [[ -f $log ]]; then
 		tail -n 100 $log > ${log}.temp
--- install/setup.sh
+++ install/setup.sh
@@ -4,20 +4,37 @@
 #   Penn Bauman <pcb8gb@virginia.edu>
 
 dir="/var/besic/"
+log="/var/log/besic/init.log"
+mkdir -p $(dirname $log)
 
 read mac < /sys/class/net/wlan0/address
 mac="$(echo ${mac:9} | sed 's/://g')"
+password=$(openssl rand -base64 32)
+
 hostnamectl set-hostname "besic-relay-$mac"
 echo "mac = \"$mac\"" > $dir/config.toml
-
-mkdir -p /var/log/besic/
+echo "password = \"$password\"" >> $dir/config.toml
 
 cp $dir/relay-git/heartbeat.sh $dir
 cp $dir/relay-git/beacon.sh $dir
+cp $dir/relay-git/urls.toml $dir
 crontab $dir/relay-git/crontab
-rm $dir/init.sh
 
-apt update
-apt upgrade
+url=$(tq .remote $dir/urls.toml)
+if (($? != 0)); then
+	echo "[$(date --rfc-3339=seconds)]: Url not found (remote)" >> $log
+	exit 1
+fi
+while true; do
+	res=$(curl "$url/api/device/new" -d "mac=$mac&type=relay&password=$password")
+	if (( $? == 0 )); then
+		break
+	fi
+	echo "[$(date --rfc-3339=seconds)]: Remote init failed ($res)" >> $log
+done
+
+echo "[$(date --rfc-3339=seconds)]: Setup complete" >> $log
+
+echo "apt update; apt -y upgrade; rm $dir/init.sh" > $dir/init.sh
 
 reboot
